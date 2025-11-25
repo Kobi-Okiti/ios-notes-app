11e64417899b74e6ddc280342bfe5b741d555af9
kobi <kobiokiti@gmail.com>
2025-11-25 15:22:52 +0100

    implemented the DELETE Request Functionality

--- Notes/Notes/Home.swift
+++ Notes/Notes/Home.swift
@@ -11,13 +11,26 @@ struct Home: View {
     
     @State var notes = [Note]()
     @State var showAdd: Bool = false
+    @State var showAlert: Bool = false
+    @State var deleteItem: Note?
+    
+    var alert: Alert{
+        Alert(title: Text("Delete"), message: Text("Are you sure you want to delete this note?"), primaryButton: .destructive(Text("Delete"), action: deleteNote), secondaryButton: .cancel())
+    }
     
     var body: some View {
         NavigationView{
             List(self.notes){ note in
                 Text(note.note)
                     .padding()
+                    .onLongPressGesture {
+                        self.showAlert.toggle()
+                        deleteItem = note
+                    }
             }
+            .alert(isPresented: $showAlert, content: {
+                alert
+            })
             .sheet(isPresented: $showAdd, onDismiss: fetchNotes, content: {
                 AddNoteView()
             })
@@ -46,6 +59,29 @@ struct Home: View {
             }
         }
         task.resume()
+    }
+    
+    func deleteNote(){
+        guard let id = deleteItem?._id else { return }
+        let url = URL(string: "http://localhost:3000/notes/\(id)")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        let task = URLSession.shared.dataTask(with: request) { data, res, err in
+            guard err == nil else {return}
+            guard let data = data else {return}
+            do{
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String : Any]{
+                    print(json)
+                }
+                    
+            }
+            catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+        
+        fetchNotes()
     }
 }
 
