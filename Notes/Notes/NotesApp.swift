6025946fe61298a2a10a1ec91423deca8d57357b
kobi <kobiokiti@gmail.com>
2025-11-25 12:33:21 +0100

    created the base ui to build upon

---
+++ Notes/Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Oghenekobiruo Okiti on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
