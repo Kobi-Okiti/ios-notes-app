3c667f40abebb9b45889331d57e08314a512b293
kobi <kobiokiti@gmail.com>
2025-11-26 18:46:06 +0100

    Integrated the update route with Patch to Update notes

---
+++ Notes/Notes/UpdateNoteView.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateNoteView.swift
+//  Notes
+//
+//  Created by Oghenekobiruo Okiti on 26/11/2025.
+//
+
+import SwiftUI
+
+struct UpdateNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateNoteView()
+}
